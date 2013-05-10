19d0cd1ed1a68a8a54358a1fff7f734c0efe93e6
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-05-09 22:15:03 -0700

    SyncLogic test also passes... Let's see what will happen in the simulation

--- test-ns3/test_sync_logic.cc
+++ test-ns3/test_sync_logic.cc
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Zhenkai Zhu <zhenkai@cs.ucla.edu>
- *         Chaoyi Bian <bcy@pku.edu.cn>
  *	   Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
@@ -31,9 +30,14 @@ using boost::test_tools::output_test_stream;
 #include "sync-logic.h"
 #include "sync-seq-no.h"
 
+#include <ns3/core-module.h>
+#include <ns3/network-module.h>
+#include <ns3/ndnSIM-module.h>
+
 using namespace std;
 using namespace boost;
 using namespace Sync;
+using namespace ns3;
 
 struct Handler
 {
@@ -71,30 +75,91 @@ struct Handler
   map<string, uint32_t> m_map;
 };
 
-BOOST_AUTO_TEST_CASE (SyncLogicTest)
+class SyncLogicFixture
 {
-  Handler h1 ("1");
-
-  SyncLogic l1 ("/bcast", bind (&Handler::wrapper, &h1, _1), bind (&Handler::onRemove, &h1, _1));
-
-  std::string oldDigest  = l1.getRootDigest();
+public:
+  SyncLogicFixture ()
+    : h1 ("1")
+    , h2 ("2")
+  {
+  }
   
-  l1.addLocalNames ("/one", 1, 2);
+  void
+  step1 ()
+  {
+    Config::SetDefault ("ns3::ndn::ForwardingStrategy::CacheUnsolicitedData", BooleanValue (true));
+    
+    Ptr<Node> node = CreateObject<Node> ();
+    ndn::StackHelper ndn;
+    ndn.Install (node);
 
-  BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
-  sleep (1);
-  BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
+    l1 = CreateObject<SyncLogic> ("/bcast", bind (&Handler::wrapper, &h1, _1), bind (&Handler::onRemove, &h1, _1));
+    l1->SetStartTime (Seconds (0));
 
-  Handler h2 ("2");
-  SyncLogic l2 ("/bcast", bind (&Handler::wrapper, &h2, _1), bind (&Handler::onRemove, &h2, _1));
+    l2 = CreateObject<SyncLogic> ("/bcast", bind (&Handler::wrapper, &h2, _1), bind (&Handler::onRemove, &h2, _1));
+    l2->SetStartTime (Seconds (1));
+    
+    node->AddApplication (l1);
+    node->AddApplication (l2);
+
+    Simulator::Schedule (Seconds (0.5), &SyncLogicFixture::step2, this);
+    
+    Simulator::Stop (Seconds (20.0));
+    Simulator::Run ();
+    Simulator::Destroy ();
+  }
   
-  sleep (1);
-  BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
-  BOOST_CHECK_EQUAL (h2.m_map.size (), 1);
-  
-  l1.remove ("/one");
-  sleep(1);
-  std::string newDigest = l1.getRootDigest();
-  BOOST_CHECK(oldDigest != newDigest);
+  void
+  step2 ()
+  {
+    oldDigest  = l1->getRootDigest ();
+    
+    l1->addLocalNames ("/one", 1, 2);
+    BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
+    Simulator::Schedule (Seconds (0.49), &SyncLogicFixture::step3, this);
+  }
 
+  void
+  step3 ()
+  {
+    BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
+
+    Simulator::Schedule (Seconds (1), &SyncLogicFixture::step4, this);
+  }
+
+  void
+  step4 ()
+  {
+    BOOST_CHECK_EQUAL (h1.m_map.size (), 0);
+    BOOST_CHECK_EQUAL (h2.m_map.size (), 1);
+    
+    l1->remove ("/one");
+    Simulator::Schedule (Seconds (1), &SyncLogicFixture::step5, this);
+  }
+
+  void
+  step5 ()
+  {
+    std::string newDigest = l1->getRootDigest ();
+    BOOST_CHECK_EQUAL (oldDigest, newDigest);
+  }
+  
+private:
+  Handler h1;
+  Ptr<SyncLogic> l1;
+
+  Handler h2;
+  Ptr<SyncLogic> l2;
+
+  std::string oldDigest;
+};
+
+
+BOOST_FIXTURE_TEST_SUITE( SyncLogicTest, SyncLogicFixture )
+
+BOOST_AUTO_TEST_CASE( test_case1 )
+{
+  step1 ();
 }
+
+BOOST_AUTO_TEST_SUITE_END()
